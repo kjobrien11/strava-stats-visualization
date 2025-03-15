7377dacbe06dc8d4ab19d07336aa067d3e18466c
KJ O'BRIEN <kjokjo219@gmail.com>
2025-03-15 16:38:52 -0400

    adds refresh token when token expires

--- strava-visualization/src/main/java/com/kjobrien/strava_visualization/POJO/StavaApi.java
+++ strava-visualization/src/main/java/com/kjobrien/strava_visualization/POJO/StavaApi.java
@@ -4,25 +4,48 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.Instant;
+import java.util.Map;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 public class StavaApi {
 
-    private String token = System.getenv("API_KEY");
     private String url = "https://www.strava.com/api/v3/athlete/activities?after=1735707600&per_page=50";
     private JsonNode jsonResposne;
-
-
-
+    private String token;
 
     public StavaApi() {
         long currentEpochSeconds = Instant.now().getEpochSecond();
         System.out.println("Epoch Seconds: " + currentEpochSeconds);
+        refrehToken();
         initializeStats();
+    }
+
+    public void refrehToken() {
+        try {
+            String jsonBody = "{\"key\":\"value\"}";
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Content-Type", "application/json");
+            headers.set("Authorization", "Bearer " + token);
+            String urlWithParams = UriComponentsBuilder.fromHttpUrl("https://www.strava.com/oauth/token")
+                    .queryParam("client_id", System.getenv("CLIENT_ID"))
+                    .queryParam("client_secret", System.getenv("CLIENT_SECRET"))
+                    .queryParam("refresh_token", System.getenv("STRAVA_REFRESH_TOKEN"))
+                    .queryParam("grant_type", "refresh_token")
+                    .toUriString();
+            HttpEntity<String> entity = new HttpEntity<>(jsonBody, headers);
+            ResponseEntity<String> response = restTemplate.exchange(urlWithParams, HttpMethod.POST, entity, String.class);
+            setToken(response);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to resfresh token", e);
+        }
     }
 
     public void initializeStats() {
@@ -46,6 +69,18 @@ public class StavaApi {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             this.jsonResposne = objectMapper.readTree(response.getBody());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Failed to parse JSON response", e);
+        }
+    }
+
+    private void setToken(ResponseEntity<String> response) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            jsonResposne = objectMapper.readTree(response.getBody());
+            token = jsonResposne.get("access_token").asText();
+            System.out.println(token);
+
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Failed to parse JSON response", e);
         }
