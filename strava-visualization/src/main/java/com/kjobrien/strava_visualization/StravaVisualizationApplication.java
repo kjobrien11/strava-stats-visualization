4c553f4b8ded9d3c9dcaf7a0c4b9cfbce3d9dff7
KJ O'BRIEN <kjokjo219@gmail.com>
2025-03-14 22:18:50 -0400

    initial commit

---
+++ strava-visualization/src/main/java/com/kjobrien/strava_visualization/StravaVisualizationApplication.java
@@ -0,0 +1,13 @@
+package com.kjobrien.strava_visualization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StravaVisualizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StravaVisualizationApplication.class, args);
+	}
+
+}
---
+++ strava-visualization/src/test/java/com/kjobrien/strava_visualization/StravaVisualizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjobrien.strava_visualization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StravaVisualizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
