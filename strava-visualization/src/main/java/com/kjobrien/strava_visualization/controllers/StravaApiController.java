2c7c7ec03cb0c68e5f604b59d0f34eafc83336ab
KJ O'BRIEN <kjokjo219@gmail.com>
2025-03-15 14:41:04 -0400

    refactors logic of get method into a service class

--- strava-visualization/src/main/java/com/kjobrien/strava_visualization/controllers/StravaApiController.java
+++ strava-visualization/src/main/java/com/kjobrien/strava_visualization/controllers/StravaApiController.java
@@ -2,6 +2,9 @@ package com.kjobrien.strava_visualization.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kjobrien.strava_visualization.services.StravaApiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -13,36 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StravaApiController {
 
+    @Autowired
+    private StravaApiService stravaApiService;
+
     @GetMapping
-    String getAthleteStats() throws JsonProcessingException {
-        RestTemplate restTemplate = new RestTemplate();
-
-        String token = System.getenv("API_KEY");
-
-        //Return 50 workouts after january 1st
-        String url = "https://www.strava.com/api/v3/athlete/activities?after=1735707600&per_page=50";
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/json");
-        headers.set("Authorization", "Bearer " + token);
-        String jsonBody = "{\"key\":\"value\"}";
-        HttpEntity<String> entity = new HttpEntity<>(jsonBody, headers);
-
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-        System.out.println(response.getBody());
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode rootNode = objectMapper.readTree(response.getBody());
-
-        int count = 0;
-        if (rootNode.isArray()) {
-            for (JsonNode activityNode : rootNode) {
-                if(activityNode.path("name").asText().endsWith("Run")){
-                    count++;
-                }
-            }
-        }
-        System.out.println("Run Count: " + count);
-        return response.getBody();
+    public String getAthleteStats() throws JsonProcessingException {
+        return stravaApiService.getAthleteStats();
     }
 
 
---
+++ strava-visualization/src/main/java/com/kjobrien/strava_visualization/services/StravaApiService.java
@@ -0,0 +1,46 @@
+package com.kjobrien.strava_visualization.services;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class StravaApiService {
+
+    public String getAthleteStats() throws JsonProcessingException {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String token = System.getenv("API_KEY");
+
+        //Return 50 workouts after january 1st
+        String url = "https://www.strava.com/api/v3/athlete/activities?after=1735707600&per_page=50";
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+        headers.set("Authorization", "Bearer " + token);
+        String jsonBody = "{\"key\":\"value\"}";
+        HttpEntity<String> entity = new HttpEntity<>(jsonBody, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        System.out.println(response.getBody());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(response.getBody());
+
+        int count = 0;
+        if (rootNode.isArray()) {
+            for (JsonNode activityNode : rootNode) {
+                if(activityNode.path("name").asText().endsWith("Run")){
+                    count++;
+                }
+            }
+        }
+        System.out.println("Run Count: " + count);
+        return response.getBody();
+    }
+}
