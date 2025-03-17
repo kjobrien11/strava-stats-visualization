05c1dafd53ddd5ae2722d2c1520c5bc3ffd63663
KJ O'BRIEN <kjokjo219@gmail.com>
2025-03-16 20:46:48 -0400

    adds workout project in dto package

---
+++ strava-visualization/src/main/java/com/kjobrien/strava_visualization/dto/Workout.java
@@ -0,0 +1,77 @@
+package com.kjobrien.strava_visualization.dto;
+
+public class Workout {
+    double distance;
+    long timeInSeconds;
+    String type;
+    String date;
+    double averageSpeed;
+    double topSpeed;
+    double averageHeartRate;
+
+    public Workout(double distance, long timeInSeconds, String type, String date, double averageSpeed, double topSpeed, double averageHeartRate) {
+        this.distance = distance;
+        this.timeInSeconds = timeInSeconds;
+        this.type = type;
+        this.date = date;
+        this.averageSpeed = averageSpeed;
+        this.topSpeed = topSpeed;
+        this.averageHeartRate = averageHeartRate;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public long getTimeInSeconds() {
+        return timeInSeconds;
+    }
+
+    public void setTimeInSeconds(long timeInSeconds) {
+        this.timeInSeconds = timeInSeconds;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getAverageSpeed() {
+        return averageSpeed;
+    }
+
+    public void setAverageSpeed(double averageSpeed) {
+        this.averageSpeed = averageSpeed;
+    }
+
+    public double getTopSpeed() {
+        return topSpeed;
+    }
+
+    public void setTopSpeed(double topSpeed) {
+        this.topSpeed = topSpeed;
+    }
+
+    public double getAverageHeartRate() {
+        return averageHeartRate;
+    }
+
+    public void setAverageHeartRate(double averageHeartRate) {
+        this.averageHeartRate = averageHeartRate;
+    }
+}
