eb8da24659f4e0f00bdb32e5d28d12bb88750abc
KJ O'BRIEN <kjokjo219@gmail.com>
2025-03-17 20:49:28 -0400

    updates refresh method

--- strava-visualization/src/main/java/com/kjobrien/strava_visualization/services/StravaApiService.java
+++ strava-visualization/src/main/java/com/kjobrien/strava_visualization/services/StravaApiService.java
@@ -9,31 +9,32 @@ import java.util.List;
 @Service
 public class StravaApiService {
 
-    StavaApi stavaApi = new StavaApi();
+    StavaApi stravaApi = new StavaApi();
 
     public List<Workout> getAthleteStats() {
-        return stavaApi.getWorkouts();
+        return stravaApi.getWorkouts();
     }
 
     public List<Workout> refreshStats(){
-        stavaApi.requestJson();
+        stravaApi.refrehToken();
+        stravaApi.requestJson();
         return getAthleteStats();
     }
 
     public double getTotalDistanceInMeters(){
-        return stavaApi.getTotalDistanceInMeters();
+        return stravaApi.getTotalDistanceInMeters();
     }
 
     public double getTotalDistanceInMiles()  {
-        return stavaApi.getTotalDistanceInMeters()/1609.0;
+        return stravaApi.getTotalDistanceInMeters()/1609.0;
     }
 
     public long getTotalWorkoutTimeInSeconds()  {
-        return stavaApi.getTotalWorkoutTimeInSeconds();
+        return stravaApi.getTotalWorkoutTimeInSeconds();
     }
 
     public int getTotalRuns()  {
-        return stavaApi.getTotalRuns();
+        return stravaApi.getTotalRuns();
     }
 
 }
